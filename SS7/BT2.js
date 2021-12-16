52ba0dcd62ba5b21cbd3109255d87b79d0906487
kiennt0810 <ngkien2468@gmail.com>
2021-12-16 16:00:37 +0700

    commit

--- SS7/BT2.js
+++ SS7/BT2.js
@@ -1,13 +1,31 @@
 function multiplyBy()
 {
-    num1 = document.getElementById("firstNumber").value;
-    num2 = document.getElementById("secondNumber").value;
+    let num1 = document.getElementById("firstNumber").value;
+    let num2 = document.getElementById("secondNumber").value;
     document.getElementById("result").innerHTML = num1 * num2;
 }
 
 function divideBy() 
 { 
-    num1 = document.getElementById("firstNumber").value;
-    num2 = document.getElementById("secondNumber").value;
+    let num1 = document.getElementById("firstNumber").value;
+    let num2 = document.getElementById("secondNumber").value;
     document.getElementById("result").innerHTML = num1 / num2;
+    if (num2 == 0) {
+        alert("Không thể chia cho 0!");
+    }
+}
+
+function addBy() 
+{ 
+    let num1 = Number(document.getElementById("firstNumber").value);
+    let num2 = Number(document.getElementById("secondNumber").value);
+    let sum = parseInt(num1) + num2;
+    document.getElementById("result").innerHTML = sum;
+}
+
+function subBy() 
+{ 
+    let num1 = document.getElementById("firstNumber").value;
+    let num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 - num2;
 }
